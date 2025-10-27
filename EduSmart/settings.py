1b42a2c9998b121c61410c7aefd3391f8194f4d6
oumaymabenbrahem <100514613+oumaymabenbrahem@users.noreply.github.com>
2025-10-27 10:57:00 +0100

    fix: Update static files configuration and add Material Design Icons files

--- EduSmart/settings.py
+++ EduSmart/settings.py
@@ -146,6 +146,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'template_front' / 'static',
+]
+
+# Whitenoise configuration
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Add whitenoise middleware
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
